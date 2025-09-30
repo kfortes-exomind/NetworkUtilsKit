9a0fb83d452eb1c06ea40f9367f2586861aad289
Kevin FORTES <kfortes@exomind.fr>
2025-09-30 14:27:38 +0200

    Updates UtilsKit dependency

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:6.2
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -11,7 +11,7 @@ let package = Package(
 	],
 	dependencies: [
 		//		.package(path: "../UtilsKit")
-		.package(url: "https://github.com/rgmc95/UtilsKit.git", from: "6.2.0"),
+		.package(url: "https://github.com/kfortes-exomind/UtilsKit", from: "6.2.1"),
 	],
 	targets: [
 		.target(
