556d7dd18c6a5cdf8d953facafe0c67a226688a1
Pierre <224536867+PCilluffo@users.noreply.github.com>
2026-01-13 11:19:36 +0100

    Update UtilsKit depencies
    - Update UtilsKit depencies

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/kfortes-exomind/UtilsKit.git",
-                 .revision("c123148"))
+                 .revision("fa37fbc"))
     ],
     targets: [
         .target(
